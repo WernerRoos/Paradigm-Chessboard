5491185e35cf7766c6cf3f4ce61c3469bdddf112
WernerRoos <werner.roos3@gmail.com>
2024-12-23 23:19:45 +0200

    first uplaod

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-primitives'), 'version' => 'e6fb55aa913a608251e1');
